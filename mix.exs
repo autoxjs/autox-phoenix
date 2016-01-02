16eaa5be2fec91b3b4b1a29fba6daecf5f940e96
Thomas Chen <foxnewsnetwork@gmail.com>
2016-01-01 21:25:07 -0800

    bumped mix version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Autox.Mixfile do
 
   def project do
     [app: :autox,
-     version: "0.0.1",
+     version: "0.1.0",
      elixir: "~> 1.0",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: compilers(Mix.env),
