c94250b362d3a2e3cd11903fed7edf55843514b4
Thomas Chen <foxnewsnetwork@gmail.com>
2016-01-01 22:20:23 -0800

    fixed mix exs

--- mix.exs
+++ mix.exs
@@ -3,10 +3,12 @@ defmodule Autox.Mixfile do
 
   def project do
     [app: :autox,
-     version: "0.1.1",
+     version: "0.1.2",
      elixir: "~> 1.0",
-     elixirc_paths: elixirc_paths(Mix.env),
-     compilers: compilers(Mix.env),
+     description: description,
+     name: "autox",
+     source_url: "https://github.com/foxnewsnetwork/autox",
+     package: package,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      aliases: aliases,
@@ -17,26 +19,23 @@ defmodule Autox.Mixfile do
   #
   # Type `mix help compile.app` for more information.
   def application do
-    [mod: {Autox.App, []},
-     applications: [:phoenix, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex, :cors_plug]]
+    [applications: [:phoenix, :fox]]
   end
 
-  # Specifies which paths to compile per environment.
-  defp elixirc_paths(:prod), do: ["lib", "web"]
-  defp elixirc_paths(_), do: ["lib", "web", "test/support"]
-
-  defp compilers(:prod), do: Mix.compilers
-  defp compilers(_), do: [:phoenix, :gettext] ++ Mix.compilers
+  defp package do
+    [maintainers: ["Thomas Chen - (foxnewsnetwork)"],
+     licenses: ["MIT"],
+     links: %{github: "https://github.com/foxnewsnetwork/autox"}]
+  end
 
   # Specifies your project dependencies.
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.1.0"},
+    [{:phoenix, "~> 1.1"},
      {:fox, "~> 0.1"},
-     {:phoenix_ecto, "~> 2.0"},
-     {:phoenix_html, "~> 2.3"},
+     {:phoenix_ecto, "~> 2.0", only: [:dev, :test]},
+     {:phoenix_html, "~> 2.3", only: [:dev, :test]},
      {:cors_plug, "~> 0.1.3", only: [:dev, :test]},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:postgrex, ">= 0.0.0", optional: true},
