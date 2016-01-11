5fc1b34e645a53ad282c545be7ad8827063a4df2
Thomas Chen <foxnewsnetwork@gmail.com>
2016-01-11 10:25:48 -0800

    fixed configs so they not broken

--- config/config.exs
+++ config/config.exs
@@ -5,36 +5,4 @@
 # is restricted to this project.
 use Mix.Config
 
-# Configures the endpoint
-config :autox, Autox.Endpoint,
-  url: [host: "localhost"],
-  root: Path.dirname(__DIR__),
-  secret_key_base: "9H7Sut9iM//9X+qAv+ARIU5uQt5liRXMW01zbIeiMnVAqgyk20JsJEtGn29jjKMN",
-  render_errors: [accepts: ~w(json)],
-  pubsub: [name: Autox.PubSub,
-           adapter: Phoenix.PubSub.PG2]
-
-# Configures Elixir's Logger
-config :logger, :console,
-  format: "$time $metadata[$level] $message\n",
-  metadata: [:request_id]
-
-config :plug, :mimes, %{"application/vnd.api+json" => ["json-api"]}
-
-# Import environment specific config. This must remain at the bottom
-# of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
-
-# Configure phoenix generators
-config :phoenix, :generators,
-  migration: true,
-  binary_id: false
-
-config :autox, Autox.Defaults,
-  repo: Autox.Repo,
-  session_header: "autox-remember-token",
-  autox_master_key: "default-master-key-change-me",
-  endpoint: Autox.Endpoint,
-  error_view: Autox.ErrorView,
-  user_class: Autox.User,
-  session_class: Autox.Session
