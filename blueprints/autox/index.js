7d79cc90a4d4d0229c29c327013a669da4f1dd49
Thomas Chen <foxnewsnetwork@gmail.com>
2016-04-05 08:56:36 -0700

    removed unnecessary blueprint addon packages

--- blueprints/autox/index.js
+++ blueprints/autox/index.js
@@ -2,11 +2,5 @@ var RSVP = require('rsvp');
 
 module.exports = {
   description: 'installs js cookie, which is needed',
-  normalizeEntityName: function() {},
-  afterInstall: function(options) {
-    return RSVP.all([
-      this.addBowerPackagesToProject([{name: "js-cookie", target: "~2.0.3"}]),
-      this.addPackageToProject("ember-moment", "4.1.0")
-    ]);
-  }
+  normalizeEntityName: function() {}
 };
